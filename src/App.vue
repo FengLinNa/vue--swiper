227c69436ec353e56115311e393401374602162e
Alina <1625998788@qq.com>
2019-07-08 17:24:25 +0800

    swiper

---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <div id="app">
+    <p>当前最热门的五大网游</p>
+    <p>
+      <span v-for="(item,index) in list" :key="index" :class="cur==index?'active':''" @click="change(index)">{{item}}</span>
+    </p>
+    <swiper :cur="cur" :change="change"/>
+  </div>
+</template> 
+
+<script>
+
+import Swiper from "./components/swiper";
+
+export default {
+  name: 'App',
+  data(){
+    return {
+      list:["英雄联盟","贪玩蓝月","守望先锋","穿越火线","绝地求生"],
+       cur:0
+    }
+  },
+  components: {
+   Swiper
+  },
+  methods:{
+    change(cur){
+      this.cur = cur
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+*{
+  padding: 0;
+  margin: 0;
+  list-style: none;
+  box-sizing: border-box;
+}
+p{
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+span{
+  &.active{
+    color:red;
+  }
+}
+</style>
---
+++ src/components/swiper.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+      <p class="curr">{{cur}}</p>
+    <ul class="swiper-container">
+      <div class="swiper-wrapper">
+        <li v-for="(item,index) in posts" :key="index" class="swiper-slide">
+          <img :src="item" alt>
+        </li>
+      </div>
+    </ul>
+  </div>
+</template>
+<script>
+export default {
+  props:["cur","change"],
+  data() {
+    return {
+      posts: [
+        "https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=1513222572,1724296872&fm=26&gp=0.jpg",
+        "https://ss2.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=119991635,3338844579&fm=26gp=0.jpg",
+        "https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=3751132997,2377190187&fm=26&gp=0.jpg",
+        "https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=3667338627,4040384263&fm=26&gp=0.jpg",
+        "https://ss1.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=1588770783,3837654158&fm=26&gp=0.jpg"
+      ]
+    };
+  },
+  mounted() {
+      const that = this;
+    this.swiper = new Swiper("ul", {
+        on:{
+            slideChange:function(cur){
+                that.change(this.activeIndex)
+            }
+        }
+    });
+  },
+  updated(){
+      this.swiper.slideTo(this.cur)
+  }
+};
+</script>
+<style>
+.curr{
+    display: none;
+}
+</style>
+
+
